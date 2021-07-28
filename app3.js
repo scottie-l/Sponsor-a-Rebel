06dc43ae90a99af4f5e331fc9893ee3e70d089f7
Scott Lease <https://github.com/scottie-l>
2021-07-28 16:58:35 -0700

    adding 3.js

---
+++ app3.js
@@ -0,0 +1,7 @@
+let UserAnswer=prompt ("Want to Sponsor a Rebel?")
+if (UserAnswer()==yes){
+    document.write("Here's How You Can Help")
+}
+if (UserAnswer()==no){
+    document.write("Sorry to hear of your oppression!")
+}
