c99d2fbef84b093d8164d9c8672c73d2764a8f1e
Scott Lease <https://github.com/scottie-l>
2021-07-29 15:20:47 -0700

    adding dynamic component

--- app3.js
+++ app3.js
@@ -1,7 +1,12 @@
-let UserAnswer=prompt ("Want to Sponsor a Rebel?")
-if (UserAnswer()==yes){
-    document.write("Here's How You Can Help")
-}
-if (UserAnswer()==no){
-    document.write("Sorry to hear of your oppression!")
-}
+function getUserAnswer(){
+    let UserAnswer = prompt("Want to Sponsor a Rebel?");
+    if (UserAnswer === "yes"){
+        document.write("Here's How You Can Help")
+    }
+    else if(UserAnswer === "no"){
+        document.write("Sorry to hear of your oppression!")
+    }
+    else if(UserAnswer !== "yes" || UserAnswer !== "no"){
+        prompt('Please Answer yes or no')}
+}    
+getUserAnswer();
