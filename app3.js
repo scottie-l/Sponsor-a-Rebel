bec66526ad606a4e801656115ed19ce7f949d2cf
Scott Lease <https://github.com/scottie-l>
2021-07-31 10:21:20 -0700

    updating code

--- app3.js
+++ app3.js
@@ -1,12 +1,19 @@
-function getUserAnswer(){
-    let UserAnswer = prompt("Want to Sponsor a Rebel?");
-    if (UserAnswer === "yes"){
-        document.write("Here's How You Can Help")
-    }
-    else if(UserAnswer === "no"){
-        document.write("Sorry to hear of your oppression!")
-    }
-    else if(UserAnswer !== "yes" || UserAnswer !== "no"){
-        prompt('Please Answer yes or no')}
-}    
-getUserAnswer();
+// function getUserAnswer(){
+//     let yesno = false;
+//     let UserAnswer = prompt("Want to Sponsor a Rebel?");
+//     console.log (UserAnswer)
+//     while(UserAnswer !== "yes" && UserAnswer !== "no"){
+   
+//         UserAnswer = prompt('Please Answer yes or no')
+//     console.log (UserAnswer)
+//     }
+//     if (UserAnswer === "yes"){
+//         document.write("Here's How You Can Help")
+//         yesno = true
+//     }
+//     else if(UserAnswer === "no"){
+//         document.write("Sorry to hear of your oppression!")
+//         yesno = true
+//     }
+// } 
+// getUserAnswer();
