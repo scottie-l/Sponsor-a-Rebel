94e69cd5023d599ba2d0d4a22a4e7f6507d9735f
Scott Lease <https://github.com/scottie-l>
2021-07-31 10:20:34 -0700

    update file

--- app2.js
+++ app2.js
@@ -1,1 +1,16 @@
-let userAge=prompt ("How old are you?")
+// function userAge(){
+//     let userAge=prompt ("How old are you?")   
+// }
+// getuserAge();
+
+function(name) {
+    return "Hello, " + name;
+}
+greet;
+function greet(name) {
+    return "Hello, " + name;
+  }
+  greet("Grace Hopper");
+
+
+
